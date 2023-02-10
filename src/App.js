d5e8546bdea813b4d56e6b35586e9e5628e3027d
Florent DUBOST <florent.dubost@bedrockstreaming.com>
2023-02-10 11:26:59 +0100

    feat(app): message olé olé

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Coucou
+          Learn React Coucou tu veux ?
         </a>
       </header>
     </div>
